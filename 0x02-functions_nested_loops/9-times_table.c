149f71b192024c2655eacaa13c512c0c0035891a
LeoFielies <leojfielies@gmail.com>
2023-03-21 03:07:15 -0700

    a+b

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - function that prints 9 times table
+ *
+ * @i: function parameter
+ *
+ * Return: always 0
+ */
+
+
+void times_table(void)
+{
+	int n;
+
+	printf("Enter an integer: ");
+	scanf("%d", &n);
+
+	if (i = 1; i <= 10; ++i)
+	{
+		printf("%d * %d = %d \n", n, i, n * i);
+	}
+	return (0);
+}
